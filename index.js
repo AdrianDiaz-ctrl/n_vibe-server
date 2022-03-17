16e3888724381623feb847b1f1cfe22dd27cd29c
Adrian Diaz <diazgabo19@gmail.com>
2022-03-16 22:09:50 -0500

    feat: integracion de registro y login

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* EXPRESS */
+const cors = require('cors')
 const express = require('express')
 const app = express();
 const session = require('express-session')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
+app.use(cors())
 
 /* CONEXION CON DB */
 const connection = require('./database/db')
@@ -20,32 +22,43 @@ app.use(session({
     saveUninitialized: true
 }))
 
-// app.get('/', (req, res) => {
-//     res.send("Hola")
-// })
+app.get('/', (req, res) => {
+    res.send("Hola")
+})
 
 app.get('/login', (req, res) => {
     res.send("login")
 })
 
 app.post('/register', async (req, res) => {
-    const user = req.body.user;
-    const name = req.body.name;
-    const rol = req.body.rol;
-    const pass = req.body.pass;
-    let password = await bcrypt.hash(pass, 8)
-    connection.query("INSERT INTO socio SET ?", {
-        user: user, 
-        name: name, 
-        rol: rol, 
-        pass: pass
-    }, async (err, res) => {
-        if(err) throw err
-        console.log("exitosa")
-    })
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const ocupacion = req.body.ocupacion;
+    const correo = req.body.correo;
+    const pass = req.body.password;
+    let passwordHash = await bcrypt.hash(pass, 8)
+
+    if(nombre.length === 0 || edad.length === 0 || ocupacion.length === 0 || correo.length === 0 || pass.length === 0){
+        res.send("Error, llene todos los campos")
+    }else{
+        connection.query("INSERT INTO socio SET ?", {
+            nombre: nombre, 
+            correo: correo, 
+            edad: edad,
+            ocupacion: ocupacion, 
+            contraseña: passwordHash
+        }, async (err, resul) => {
+            if(err){console.log(err)}
+            else{
+                console.log("todo bien")
+                res.send('registro CORRECT')
+            }
+            
+        })
+    }
 })
 
-app.post('/auth', async (req, res) => {
+app.post('/login', async (req, res) => {
     const user = req.body.user;
     const pass = req.body.pass;
 
@@ -63,17 +76,17 @@ app.post('/auth', async (req, res) => {
     }
 })
 
-app.get('/', (req, res) => {
-    if(req.session.loggedin){
-        res.render('index', {
-            login: true
-        })
-    }else{
-        res.render('index', {
-            login: false
-        })
-    }
-})
+// app.get('/', (req, res) => {
+//     if(req.session.loggedin){
+//         res.render('index', {
+//             login: true
+//         })
+//     }else{
+//         res.render('index', {
+//             login: false
+//         })
+//     }
+// })
 
 app.get('/logout', (req, res) => {
     req.session.destroy(()=>{
